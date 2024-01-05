8f0c7a904dceabde7426fd330043eb85e7a8d256
omjadhav1910 <sonujadhav7020@gmail.com>
2024-01-05 18:42:05 +0530

    om

---
+++ progression 1/app.js
@@ -0,0 +1,24 @@
+// // const container = document.getElementById('react-container');
+
+// // ReactDOM.render("Hello I am OM",container)
+
+// const Container = () => {
+//     return React.createElement(`div`, null, `Hey Kalvians! Welcome to react ` ,
+//     React.createElement(`div`, null, `Let's rock and roll `)
+//     );
+// }
+// const container = document.getElementById('react-container');
+// ReactDOM.render( React.createElement(Container),container);
+class ReactContainer extends React.Component{
+    // constructor - to initialize the state 
+  
+    // render method to render the react dom 
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  const container = document.getElementById('react-container');
+  ReactDOM.render(React.createElement(ReactContainer),container);
